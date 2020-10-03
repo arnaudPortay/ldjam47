807048ae37b5ae722fd7c57e5559b1a16e627439
Thomas PITIOT <Brainstorm67@users.noreply.github.com>
2020-10-03 17:21:03 +0200

    Creation des squelettes de states
    Creation des squelettes de states correspondant au biome

---
+++ NormalBill/NormalBill.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+class_name PlateformerPlayer
+
+# Declare member variables here. Examples:
+export var can_move: bool = true
+export var can_swim: bool = false
+export var can_climb: bool = false
+export var can_glide: bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
