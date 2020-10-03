0303145291fc2ed9757dc67a05895925d4c460f0
arnaudPortay <arnaud.portay@telecom-st-etienne.fr>
2020-10-03 18:32:18 +0200

    Ajout scene de touche activable

---
+++ Generics/Key/Key.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+export(String) var binding_action_string
+export(StreamTexture) var key_unpressed_texture : StreamTexture setget set_unpressed_texture
+export(StreamTexture) var key_pressed_texture : StreamTexture
+
+onready var sprite := $KeySprite
+
+var is_action_done : bool = false
+
+################################################################################
+
+func _ready():
+	if sprite:
+		sprite.set_texture(key_unpressed_texture)
+
+################################################################################
+
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	if Input.is_action_just_released(binding_action_string) and not is_action_done:
+		is_action_done = true
+		sprite.set_texture(key_pressed_texture)
+
+################################################################################
+
+func set_unpressed_texture(texture):
+	key_unpressed_texture = texture
+	if sprite:
+		sprite.set_texture(key_unpressed_texture)
+
+################################################################################
