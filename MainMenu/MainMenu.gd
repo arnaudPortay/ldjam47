c76ea91afefd8f3f7bfbfc8d7ae19d7be90e6cc5
arnaudPortay <arnaud.portay@telecom-st-etienne.fr>
2020-10-03 17:05:16 +0200

    Base de menu principal

---
+++ MainMenu/MainMenu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+################################################################################
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+################################################################################
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://MainScene/MainScene.tscn")
+
+################################################################################
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
