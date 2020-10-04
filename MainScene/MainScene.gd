90a99939c18ff8b6efbf36ee6a1c6086bc3590df
arnaudPortay <arnaud.portay@telecom-st-etienne.fr>
2020-10-04 23:30:54 +0200

    MainScene anim fonctionne stretched

--- MainScene/MainScene.gd
+++ MainScene/MainScene.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 export(Array, String) var level_names = ["Mountain"]
-export(Array, Vector2) var animated_positions = [Vector2(252, 150), Vector2(252, 273), Vector2(368, 532)]
 export(Vector2) var last_position
 var animated_state = 0
 var last_elapsed_animation = 0
@@ -11,27 +10,32 @@ const MAX_DELAY_ANIMATION = 1
 const MAX_DELAY_WIN = 1;
 var won_the_game = false
 
+onready var anim_node = $CanvasLayer/Animations
+onready var bill = $CanvasLayer/CursorBill
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CursorBill.do_handle_inputs = false
+	bill.position = anim_node.get_child(0).rect_position
+	bill.do_handle_inputs = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	elapsed += delta
-	if elapsed - last_elapsed_animation > MAX_DELAY_ANIMATION and animated_state < len(animated_positions):
+	if elapsed - last_elapsed_animation > MAX_DELAY_ANIMATION and animated_state < anim_node.get_child_count():
 		last_elapsed_animation = elapsed
-		$CursorBill.position = animated_positions[animated_state]
+		bill.position = anim_node.get_child(animated_state).rect_position
 		animated_state += 1
-		if animated_state == len(animated_positions):
-			$CursorBill.do_handle_inputs = true
+		if animated_state == anim_node.get_child_count():
+			bill.do_handle_inputs = true
 			
 	if not won_the_game and GameStats.last_succeded_level == len(level_names) - 1:
 		won_the_game = true
-		$CursorBill.position = last_position
+		bill.position = last_position
 		last_elapsed_win = elapsed
 		
 	if won_the_game and elapsed - last_elapsed_win > MAX_DELAY_WIN:
 		last_elapsed_win = elapsed
+# warning-ignore:return_value_discarded
 		get_tree().change_scene("res://EndMenu/EndMenu.tscn")
 		
 # DEBUG:
@@ -46,5 +50,6 @@ func check_level_accessibility(level_index):
 
 func _on_CursorBill_try_entering_level(level_index):
 	if check_level_accessibility(level_index):
+# warning-ignore:return_value_discarded
 		get_tree().change_scene("res://Levels/" + level_names[level_index] + "/" + level_names[level_index] + ".tscn")
 		
