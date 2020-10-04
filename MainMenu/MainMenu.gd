f0bd7c4affe5ad42c57af508e1e0a7fe1cb2fa5d
Thomas PITIOT <Brainstorm67@users.noreply.github.com>
2020-10-05 00:24:14 +0200

    music loops allowed

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -3,6 +3,7 @@ extends Control
 
 ################################################################################
 func _ready():
+	MusicController.loop_between(0.0,MusicController.t_end_intro)
 	MusicController.playMusic(MusicController.m_main_music)
 	$VBoxContainer/StartButton.grab_focus()
 
@@ -10,6 +11,7 @@ func _ready():
 
 func _on_StartButton_pressed():
 # warning-ignore:return_value_discarded
+	MusicController.loop_between(MusicController.t_start_beat)
 	get_tree().change_scene("res://MainScene/MainScene.tscn")
 
 ################################################################################
