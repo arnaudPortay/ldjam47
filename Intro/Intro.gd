e340f3220764917e633b38d40a9bd3c54ea7bbb9
arnaudPortay <arnaud.portay@telecom-st-etienne.fr>
2020-10-06 00:04:10 +0200

    video intro

---
+++ Intro/Intro.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+func _on_VideoPlayer_finished():
+	MusicController.loop_between(MusicController.t_start_beat)
+	MusicController.playMusic(MusicController.m_main_music)
+	get_tree().change_scene("res://MainScene/MainScene.tscn")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		MusicController.loop_between(MusicController.t_start_beat)
+		MusicController.playMusic(MusicController.m_main_music)
+		get_tree().change_scene("res://MainScene/MainScene.tscn")
