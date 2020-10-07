cf4e523e5f792ed3c3338cf4accd8a8ff2cb3523
Thomas PITIOT <Brainstorm67@users.noreply.github.com>
2020-10-08 00:54:01 +0200

    cpp servant d'image de fond

---
+++ Sources/int main ().cpp
@@ -0,0 +1,29 @@
+int main ()
+{
+    bool be_a_noob,taste_sand, get_high = false;
+
+    virtualise_bill();
+    while (true)
+    {
+        be_a_noob = explore_mountain();
+        if (be_a_noob) taste_sand = explore_desert();
+        if (taste_sand) get_high = explore_pit();
+    
+    
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
